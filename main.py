b8bd6a75a161ffc640eb1e07955bd8f61779ecb6
shankhalam <shankhalamh@gmail.com>
2025-03-12 20:08:59 +0400

    Add UI implementation for password manager with tkinter

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from tkinter import messagebox
 import random
 import pyperclip as pc
 import json
-
-# --------------------------VARIABLES------------------------------------------- #
-BLUE = "#632780"
-DARK_BLUE = "#352962"
-WHITE = "#FFFFFF"
+from ui import *
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -104,43 +100,3 @@ def find_data():
             else:
                 messagebox.showwarning(title="No Entry", message=f"No details for {web_entry} Found.")
 
-
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("New Pass - Password Manager")
-window.config(padx=50, pady=50)
-window.resizable(False, False)
-# window.iconbitmap("assets/icon.ico")
-
-# Canvas
-canvas = Canvas(width=200, height=200)
-# logo_image = PhotoImage(file="assets/newpass_img.png")
-canvas.create_image(100, 100)
-canvas.grid(row=0, column=1, pady=10)
-
-# Labels
-website = Label(text="Website :")
-website.grid(row=1, column=0)
-username = Label(text="Email/Username :")
-username.grid(row=2, column=0)
-password = Label(text="Password :")
-password.grid(row=3, column=0)
-
-# Entries
-website_entry = Entry(width=33)
-website_entry.grid(row=1, column=1)
-website_entry.focus()
-user_entry = Entry(width=52)
-user_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=33)
-password_entry.grid(row=3, column=1)
-
-# Buttons
-generate_pass = Button(text="Generate Password", command=generate_password, bg=DARK_BLUE, fg=WHITE)
-generate_pass.grid(row=3, column=2)
-save_data = Button(text="Save Data", width=44, bg=BLUE, fg=WHITE, command=save)
-save_data.grid(row=4, column=1, columnspan=2, pady=10)
-search_data = Button(text="Search Website", command=find_data, bg=DARK_BLUE, fg=WHITE, width=14)
-search_data.grid(row=1, column=2)
-
-window.mainloop()
